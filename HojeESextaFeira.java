bfef17f688d057d9d9bfda0b168e08e71a6de095
DiogoDias15 <32169122+DiogoDias15@users.noreply.github.com>
2017-11-24 15:01:26 +0000

    Add files via upload

---
+++ HojeESextaFeira.java
@@ -0,0 +1,12 @@
+
+package hoje.e.sexta.feira;
+
+public class HojeESextaFeira {
+
+   
+    public static void main(String[] args) {
+        {final String mensagem="hoje é sexta-feiras";
+        for(int I=1;I<=5;I++)
+        System.out.println(mensagem);}}
+    
+}
