80604aa3205129ba8f30f463a1e3a274ebac920a
DiogoDias15 <32169122+DiogoDias15@users.noreply.github.com>
2017-12-15 12:18:46 +0000

    Add files via upload

---
+++ MultiplicaçãoRussa2.java
@@ -0,0 +1,52 @@
+
+package multiplicaçãorussa2;
+import java.util.Scanner;
+public class MultiplicaçãoRussa2 {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x,y;
+        Scanner LerS = new Scanner(System.in);
+        System.out.print("Qual o multiplicador?");
+        x=LerS.nextInt();
+        System.out.print("Qual o multiplicando?");
+        y= LerS.nextInt();
+        System.out.print("x="+x);
+        System.out.println();
+        System.out.print("y="+y);
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+    }
+    
+}
